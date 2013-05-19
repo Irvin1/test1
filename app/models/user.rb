dc633deb12f3d0ba6798b19c768aa813d1ecfaae
Irvin <irva.stevic@gmail.com>
2013-05-19 14:39:49 +0200

    hoho

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   has_many :articles, foreign_key: "author", dependent: :destroy, primary_key:"name"
   has_secure_password
-  attr_accessible :email, :name, :password, :password_confirmation, :admin
+  attr_accessible :email, :name, :password, :password_confirmation, :admin, :avatar
+  
+  has_attached_file :avatar, :url  => "/assets/avatars/:id/:basename.:extension",
+                  :path => ":rails_root/public/assets/avatars/:id/:basename.:extension"
+
+  validates_attachment_presence :avatar
+  validates_attachment_size :avatar, :less_than => 5.megabytes
+  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png']
   
   validates :name, :email, :presence => true
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130517204600) do
+ActiveRecord::Schema.define(:version => 20130519120516) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -25,10 +25,14 @@ ActiveRecord::Schema.define(:version => 20130517204600) do
     t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.string   "password_digest"
     t.boolean  "admin"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
